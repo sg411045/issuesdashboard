f98953d8adb82efb6e73215d2938db2b9be8c15d
Swami Gopalakrishnan <swaminathan.g@gmail.com>
2018-07-01 10:56:08 +0530

    updated for backend api

--- src/main/java/com/example/issuesdashboard/IssuesdashboardApplication.java
+++ src/main/java/com/example/issuesdashboard/IssuesdashboardApplication.java
@@ -1,9 +1,12 @@
 package com.example.issuesdashboard;
 
+import com.example.issuesdashboard.github.GithubProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GithubProperties.class)
 public class IssuesdashboardApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/issuesdashboard/github/GithubClient.java
+++ src/main/java/com/example/issuesdashboard/github/GithubClient.java
@@ -1,9 +1,20 @@
 package com.example.issuesdashboard.github;
 
+import org.springframework.boot.devtools.remote.client.HttpHeaderInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class GithubClient {
@@ -12,12 +23,37 @@ public class GithubClient {
 
   private final RestTemplate restTemplate;
 
-  public GithubClient(RestTemplateBuilder restTemplateBuilder) {
-    this.restTemplate = restTemplateBuilder.build();
+  public GithubClient(RestTemplateBuilder restTemplateBuilder,GithubProperties properties) {
+
+    this.restTemplate = restTemplateBuilder
+        .additionalInterceptors(new GithubAppTokenInterceptor(properties.getToken()))
+        .build();
   }
 
   public ResponseEntity<RepositoryEvent[]> fetchEvents(String orgName, String repoName) {
 
     return restTemplate.getForEntity(EVENT_ISSUES_URL, RepositoryEvent[].class, orgName, repoName);
+  }
+
+  private static class GithubAppTokenInterceptor implements ClientHttpRequestInterceptor {
+
+    private final String token;
+
+    GithubAppTokenInterceptor(String token) {
+      this.token = token;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest httpRequest,
+        byte[] bytes,
+        ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+
+      if(StringUtils.hasText(this.token)) {
+        byte[] basicAuthValue = this.token.getBytes(StandardCharsets.UTF_8);
+        httpRequest.getHeaders().set(HttpHeaders.AUTHORIZATION,
+            "Basic " + Base64Utils.encodeToString(basicAuthValue));
+      }
+      return clientHttpRequestExecution.execute(httpRequest, bytes);
+    }
   }
 }
